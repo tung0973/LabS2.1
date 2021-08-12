59c466a718378183165c943677da6d241066bf20
Thanh Tung <tung09733@gmail.com>
2021-08-12 08:19:38 +0700

    Add project files.

---
+++ LabS2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabS2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name : ");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Address :");
+            String Address = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Phone :");
+            String Phone = Console.ReadLine();
+
+            Console.WriteLine("Your Name Is :"+ name);
+            Console.WriteLine("Your Address Is :" + Address);
+            Console.WriteLine("Your Phone Is : " + Phone);
+        }
+    }
+}
